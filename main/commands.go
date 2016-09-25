b904e76e725c3acf418a945b673944fd89b9ec07
mmoghaddam385 <m.moghaddam385@outlook.com>
2016-09-25 02:21:59 -0400

    Added runner framework and help command

---
+++ main/commands.go
@@ -0,0 +1,94 @@
+package main
+
+import "fmt"
+
+type command struct {
+	helpText []string
+	execute  func(...string)
+}
+
+// list of command names
+var commands map[string]command
+
+// I can't initialize the comands map to have this because there would be circular references
+func initCommands() {
+	commands = make(map[string]command)
+	commands["help"] = command{[]string{"Show help message",
+		"Optional argument: command name"}, showHelpCommand}
+
+	commands["quit"] = command{[]string{"Quits the program"}, nil}
+	commands["exit"] = command{[]string{"Quits the program"}, nil}
+
+	commands["search"] = command{[]string{"Search for a record",
+		"Required argument: query string",
+		"This command returns the names of any record whose ",
+		"site name contains the query string"}, searchCommand}
+
+	commands["view"] = command{[]string{"View the contents of a record",
+		"Required argument: record sitename",
+		"This command displays the attributes of the record in plain text",
+		"The fields are masked after enter is pressed and the command exits"}, viewRecordCommand}
+
+	commands["delete"] = command{[]string{"Delete a record permanently",
+		"Required argument: record sitename"}, deleteRecordCommand}
+
+	commands["rm"] = command{[]string{"See 'delete'"}, deleteRecordCommand}
+
+	commands["create"] = command{[]string{"Create and save a new record",
+		"No required arguments; you will be prompted for values"}, createRecordCommand}
+
+	commands["new"] = command{[]string{"See 'create'"}, createRecordCommand}
+
+	commands["change-password"] = command{[]string{"Change Password",
+		"No required arguments; you will be prompted for values"}, changePasswordCommand}
+}
+
+func showHelpCommand(args ...string) {
+	// if arguments are supplied, display help text only for those commands
+	if len(args) > 0 {
+		for _, arg := range args {
+			cmd := commands[arg]
+
+			fmt.Printf("%v:\n", arg)
+			if len(cmd.helpText) > 0 {
+				printHelpText(cmd.helpText)
+			} else {
+				printHelpText([]string{"Command not found"})
+			}
+
+			fmt.Println()
+		}
+	} else { // no args? just print em all!
+		for name, cmd := range commands {
+			fmt.Printf("%v:\n", name)
+			printHelpText(cmd.helpText)
+			fmt.Println()
+		}
+	}
+}
+
+func printHelpText(helpText []string) {
+	for _, textLine := range helpText {
+		fmt.Printf("\t\t\t%v\n", textLine)
+	}
+}
+
+func searchCommand(args ...string) {
+	fmt.Println("Not yet implemented")
+}
+
+func viewRecordCommand(args ...string) {
+	fmt.Println("Not yet implemented")
+}
+
+func deleteRecordCommand(args ...string) {
+	fmt.Println("Not yet implemented")
+}
+
+func createRecordCommand(args ...string) {
+	fmt.Println("Not yet implemented")
+}
+
+func changePasswordCommand(args ...string) {
+	fmt.Println("Not yet implemented")
+}
--- main/runner.go
+++ main/runner.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
 //TODO: Ensure that the program is writting to stdout so that carriage returns work properly
 
 /*******************************************************************************************/
@@ -15,4 +22,41 @@ func main() {
 	authenticate()
 
 	loadRecords()
+
+	startMainLoop()
+}
+
+// startMainLoop starts the main logic loop that runs the program
+func startMainLoop() {
+	initCommands()
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for getInput(scanner) {
+	}
+}
+
+// getInput will get input and execute the command
+// it will return true if we should keep going, false if we should quit
+func getInput(scanner *bufio.Scanner) bool {
+	fmt.Print("Enter command (try 'help'): ")
+
+	scanner.Scan()
+	input := scanner.Text()
+
+	splitInput := strings.Split(input, " ")
+	commandName := splitInput[0]
+	commandArgs := splitInput[1:]
+
+	cmd := commands[commandName]
+
+	if cmd.execute != nil {
+		cmd.execute(commandArgs[:]...)
+	} else if strings.Compare(commandName, "quit") == 0 || strings.Compare(commandName, "exit") == 0 {
+		return false
+	} else {
+		fmt.Printf("Command '%v' unrecognized, try 'help'\n", commandName)
+	}
+
+	return true
 }
